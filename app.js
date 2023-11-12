0c183efce5dad0767016d916331f9ef37682dd22
piyush.g <piyush.g@naukri.com>
2023-11-12 16:08:40 +0530

    testing the cookie functionality

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ app.get("/random", (req, res) => {
 
   // Set the "x-random" header in the response
   res.setHeader("x-random", randomNumber);
-  res.setHeader("x-cookie", "deviceId=7778921-31123;isUstSaved=true;");
+  res.setHeader("x-deviceId-cookie", "deviceId=7778921-31123");
+  res.setHeader("x-isUstSaved-cookie", true);
   res.json({ result: randomNumber });
 });
 
